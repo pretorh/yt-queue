b1c75491498283ef6965dd3805ad70019537512f
Hendri Pretorius <pretorh@gmail.com>
2023-10-29 13:21:38 +0200

    tests: add simplified mock response data for extract_info

--- tests/mocks.py
+++ tests/mocks.py
@@ -30,3 +30,21 @@ def mock_yt_dlp(monkeypatch, extract_info=None):
         )
 
     monkeypatch.setattr(yt_dlp, "YoutubeDL", create_mock)
+
+def response_extract_info(video_count=1):
+    " returns a simplified version of playlist data for extract_info "
+    video_responses=[]
+    for i in range(0, video_count):
+        video_responses.append(response_video_playlist_info(i))
+
+    return {
+        'id': 'PL-id1',
+        'entries': video_responses,
+    }
+
+def response_video_playlist_info(vid):
+    " returns a simplified response for a video in a playlist for extract_info "
+    return {
+        'id': f'{vid}',
+        'url': f'https://example.com/video/v{vid}',
+    }
