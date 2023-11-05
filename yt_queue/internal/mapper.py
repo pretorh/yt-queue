a608b0ad41863bae8123da8c5ead0181b68c9d42
Hendri Pretorius <pretorh@gmail.com>
2023-11-05 10:16:29 +0200

    extract map_entry_to_video and add test for this function directly

--- yt_queue/internal/mapper.py
+++ yt_queue/internal/mapper.py
@@ -1,12 +1,19 @@
-def map_and_merge(entry, videos):
+def map_entry_to_video(entry):
     if entry is None:
+        return None
+
+    return {
+            'id': entry['id'],
+            'url': entry['url'],
+    }
+
+def map_and_merge(entry, videos):
+    video = map_entry_to_video(entry)
+    if video is None:
         return
 
     existing = [x for x in videos if x['id'] == entry['id']]
     if any(existing):
         existing[0]['url'] = entry['url']
     else:
-        videos.append({
-            'id': entry['id'],
-            'url': entry['url'],
-        })
+        videos.append(video)
