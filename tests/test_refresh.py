773f11fb961d8c5fda49958af90c7fcd8c278947
Hendri Pretorius <pretorh@gmail.com>
2023-12-06 20:17:13 +0200

    tests: extract fixture for temp file setup

--- tests/test_refresh.py
+++ tests/test_refresh.py
@@ -1,10 +1,16 @@
+import pytest
 import yt_queue
 from .mocks import mock_yt_dlp, response_extract_info, append_to_response
 
-def test_refresh_skips_none_videos(tmp_path, monkeypatch):
+@pytest.fixture(name='path_to_created_file')
+def create_simple_file(tmp_path):
     file = tmp_path / 'info.json'
     with open(file, 'w', encoding='utf-8') as f:
         f.write('{ "url": "https://example.com/playlist/1" }')
+    return file
+
+def test_refresh_skips_none_videos(path_to_created_file, monkeypatch):
+    file = path_to_created_file
 
     response = response_extract_info(video_count=3)
     append_to_response(response, None)
