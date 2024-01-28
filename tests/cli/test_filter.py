9baf00a2968b7d11e7bd0b35b8d1b37a1cd9c5f8
Hendri Pretorius <pretorh@gmail.com>
2024-01-28 12:30:30 +0200

    tests: add python tests for cli parsing (#27)
    add tests in python for cli arguments parsing and results. this is
    similar to the cli function tests, but the input is a cli string
    
    fixes #25

---
+++ tests/cli/test_filter.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+import json
+import pytest
+from yt_queue import cli
+from ..mocks import data_dict
+
+@pytest.fixture(name='file_with_some_data')
+def create_info_file(tmp_path):
+    file = Path(tmp_path, 'info.json')
+    with open(file, 'w', encoding='utf-8') as f:
+        json.dump(data_dict(), f)
+    return file
+
+def test_filter_without_args_returns_all(file_with_some_data, capsys):
+    cli(f"filter {file_with_some_data}".split())
+    captured = capsys.readouterr()
+    assert captured.out == "idA\nidB\nidC\n"
+
+def test_filter_status(file_with_some_data, capsys):
+    cli(f"filter {file_with_some_data} --status test".split())
+    captured = capsys.readouterr()
+    assert captured.out == "idB\nidC\n"
+
+def test_filter_no_status(file_with_some_data, capsys):
+    cli(f"filter {file_with_some_data} --no-status".split())
+    captured = capsys.readouterr()
+    assert captured.out == "idA\n"
+
+def test_filter_min_duration(file_with_some_data, capsys):
+    cli(f"filter {file_with_some_data} --min-duration 60".split())
+    captured = capsys.readouterr()
+    assert captured.out.find("idB") == -1
+
+def test_filter_max_duration(file_with_some_data, capsys):
+    cli(f"filter {file_with_some_data} --max-duration 60".split())
+    captured = capsys.readouterr()
+    assert captured.out.find("idB") != -1
+
+def test_filter_title(file_with_some_data, capsys):
+    regex = '[BC]$'
+    cli(f"filter {file_with_some_data} --title {regex}".split())
+    captured = capsys.readouterr()
+    assert captured.out == "idB\nidC\n"
