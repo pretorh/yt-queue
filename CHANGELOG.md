d970bf2627b09c3b917e707483562fdd4520a061
Hendri Pretorius <pretorh@gmail.com>
2024-01-31 17:20:56 +0200

    bump to version 0.7.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## unreleased
+## 0.7.1
 
 ### Fixed
 
--- yt_queue/__init__.py
+++ yt_queue/__init__.py
@@ -7,7 +7,7 @@ from .filters import filter_videos
 from .utils.loggers import StdLogger
 from .utils.time import is_stale
 
-VERSION = '0.7.0'
+VERSION = '0.7.1'
 _fullname = f"yt-queue {VERSION}"
 
 # utils
