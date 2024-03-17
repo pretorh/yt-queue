a72cde099a1fca4a93c7477da98b0a62b132bfcd
Hendri Pretorius <pretorh@gmail.com>
2024-03-17 10:33:24 +0200

    dev: add helper script to bump version

---
+++ dev/version-bump.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+new_version=${1?'new version number missing'}
+
+sed -i "s/^## unreleased$/## $new_version/" CHANGELOG.md
+sed -i "s/^VERSION = '.*'$/VERSION = '$new_version'/" yt_queue/__init__.py
+
+git add --patch CHANGELOG.md yt_queue/__init__.py
+git commit -m "bump to version $new_version"
+read -r -p "enter for diff to last release"
+git diff --staged "$(git tag | tail -n1)"
+
+echo "https://github.com/pretorh/yt-queue/releases/new"
+echo "tag and release title: v$new_version"
