70ed87681af1c302391b635069bf7c5c8d03863b
Hendri Pretorius <pretorh@gmail.com>
2024-07-21 09:36:23 +0200

    bump to version 0.10.2

--- yt_queue/__init__.py
+++ yt_queue/__init__.py
@@ -7,7 +7,7 @@ from .filters import filter_videos
 from .utils.loggers import StdLogger
 from .utils.time import is_stale
 
-VERSION = '0.10.1'
+VERSION = '0.10.2'
 _fullname = f"yt-queue {VERSION}"
 
 # utils
