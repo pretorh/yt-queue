dda0f597086f89cf59e310dcdec0c9fc2450c3d9
Hendri Pretorius <pretorh@gmail.com>
2024-08-04 08:48:53 +0200

    dev: use `git describe` to get the latest tag

--- dev/version-bump.sh
+++ dev/version-bump.sh
@@ -9,7 +9,7 @@ sed -i.orig "s/^VERSION = '.*'$/VERSION = '$new_version'/" yt_queue/__init__.py
 git add --patch CHANGELOG.md yt_queue/__init__.py
 git commit -m "bump to version $new_version"
 read -r -p "enter for diff to last release"
-git diff --staged "$(git tag | tail -n1)"
+git diff --staged "$(git describe --tags --abbrev=0)"
 
 echo "https://github.com/pretorh/yt-queue/releases/new"
 echo "tag and release title: v$new_version"
