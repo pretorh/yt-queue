743b554954a7b0f6df49b621659b754a91554fe4
Hendri Pretorius <pretorh@gmail.com>
2024-10-27 16:24:24 +0200

    bump to version 0.11.3 for yt-dlp update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## 0.11.3
+
+### Changed
+
+- Updated `yt-dlp` to `2024.10.22`
+
 ## 0.11.2
 
 ### Changed
--- yt_queue/__init__.py
+++ yt_queue/__init__.py
@@ -7,7 +7,7 @@ from .filters import filter_videos
 from .utils.loggers import StdLogger
 from .utils.time import is_stale
 
-VERSION = '0.11.2'
+VERSION = '0.11.3'
 _fullname = f"yt-queue {VERSION}"
 
 # utils
