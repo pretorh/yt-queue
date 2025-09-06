f1da1db4c58e2c9b9a48377d8f4f6c9916e2f6a0
Hendri Pretorius <pretorh@gmail.com>
2025-09-06 07:33:37 +0200

    Dev: add option to bump patch number

--- dev/version-bump.sh
+++ dev/version-bump.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 set -e
+git fetch
 
 new_version=${1?'new version number missing'}
+old_version=$(grep "VERSION = " yt_queue/__init__.py | awk -F\' '{print $2}')
+if [ "$new_version" == "patch" ] ; then
+  new_version=$(echo "$old_version" | awk -F. '{print $1"."$2"."$3+1}')
+fi
 
+echo "Bump from $old_version -> $new_version"
 sed -i.orig "s/^## unreleased$/## $new_version/" CHANGELOG.md
 sed -i.orig "s/^VERSION = '.*'$/VERSION = '$new_version'/" yt_queue/__init__.py
 
