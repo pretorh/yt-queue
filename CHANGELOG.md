1fe68a8801e1f95b5630ede5f15a3933766a4671
Hendri Pretorius <pretorh@gmail.com>
2026-07-12 14:50:25 +0200

    bump to version 0.13.8 for yt-dlp update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## 0.13.8
+
+### Changed
+
+- Updated `yt-dlp` to `2026.07.04`
+
 ## 0.13.7
 
 ### Changed
--- yt_queue/__init__.py
+++ yt_queue/__init__.py
@@ -7,7 +7,7 @@ from .filters import filter_videos
 from .utils.loggers import StdLogger
 from .utils.time import is_stale
 
-VERSION = '0.13.7'
+VERSION = '0.13.8'
 _fullname = f"yt-queue {VERSION}"
 
 # utils
